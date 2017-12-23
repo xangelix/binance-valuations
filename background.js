9d6b6f0d2b62427c568dc9ec1f3a08c41fdb5de3
Cody Neiman <neiman1010222@gmail.com>
2017-12-22 21:37:45 -0500

    Comments added to background.js

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+//Tells chrome to execute content.js when the extension button is pressed
+
 chrome.browserAction.onClicked.addListener(function(tab) {
    chrome.tabs.executeScript(null, {file: "content.js"});
 });
