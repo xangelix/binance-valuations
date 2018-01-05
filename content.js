af0252bd4ff60ef7cb39825bb55b8022d1e29cda
Cody Neiman <neiman1010222@gmail.com>
2018-01-04 21:41:04 -0500

    WIP support for trading view along with upcoming features of 3.0

--- content.js
+++ content.js
@@ -13,15 +13,56 @@ var HttpClient = function() {
     };
 };
 
+function getCurrency(currency) {
+const symbols = {
+  USD: '$',
+  EUR: '€',
+  GBP: '£',
+  INR: '₹',
+  AUD: 'A$',
+  CAD: 'C$',
+  JPY: 'J￥',
+  CNY: 'C￥'
+};
+return symbols[currency];
+}
+
 // Collection of website information by class
 var names = document.getElementsByClassName('coin ng-binding');
 var amounts = document.getElementsByClassName('total f-right ng-binding');
 var btcs = document.getElementsByClassName('equalValue f-right ng-binding');
 var namesIndex = [];
+var index;
 var currency = 'USD';
+var prefix = `<button class="btn btn-success" onclick=" window.open('https://www.tradingview.com/chart/?symbol=BITFINEX%3A`;
+var suffix1 = `','_blank')">`;
+var suffix2 = `</button>`;
 
+/*
+<!-- TradingView Widget BEGIN -->
+<script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+new TradingView.widget({
+  "width": 980,
+  "height": 610,
+  "symbol": "",
+  "interval": "D",
+  "timezone": "Etc/UTC",
+  "theme": "Light",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "allow_symbol_change": true,
+   "news": [
+		"stocktwits"
+	  ],
+  "hideideas": true
+});
+</script>
+<!-- TradingView Widget END -->
+*/
 // Test if prices have already been added
-if (amounts[1].innerHTML.includes('-')) {
+if (amounts[1].innerHTML.includes('" class="btn btn-success"')) {
   for (var i = 1; i < amounts.length; i++) {
     // Removal of previous prices
     amounts[i].innerHTML = amounts[i].innerHTML.substring(0, amounts[i].innerHTML.indexOf('-'));
@@ -53,30 +94,19 @@ function parseP(coinName, coinValue) {
   coinValue = coinValue.substr(0, coinValue.length - 1);
   //console.log('DEBUG: Coin Name: ' + coinName.replace(/<(?:.|\n)*?>/gm, ''));
   //console.log('DEBUG: Coin Value: ' + parseFloat(coinValue));
-  var index = indexFinder(coinName);
-  //console.log('DEBUG: Coin Amounts: ' + parseFloat(amounts[index].innerHTML));
-  var output = parseFloat(coinValue) * parseFloat(btcs[index + 1].innerHTML);
+  index = indexFinder(coinName);
+  if (parseFloat(btcs[index + 1].innerHTML) > 0) {
+    //console.log('DEBUG: Coin Amounts: ' + parseFloat(amounts[index].innerHTML));
+    var output = parseFloat(coinValue) * parseFloat(btcs[index + 1].innerHTML);
 
-  function getCurrency(currency) {
-  const symbols = {
-    USD: '$',
-    EUR: '€',
-    GBP: '£',
-    INR: '₹',
-    AUD: 'A$',
-    CAD: 'C$',
-    JPY: 'J￥',
-    CNY: 'C￥'
-  };
-  return symbols[currency];
+    output = getCurrency(currency) + output.toFixed(2);
+    //console.log('TOTAL: ' + output);
+    // To-do: Add tradingview graphs to changed elements
+    // Final posting of data
+    amounts[index + 1].innerHTML += prefix + coinName.replace(/<(?:.|\n)*?>/gm, '') + currency + suffix1 + output + suffix2;
   }
-
-  output = ' - ' + getCurrency(currency) + output.toFixed(2);
-  //console.log('TOTAL: ' + output);
-  // To-do: Add tradingview graphs to changed elements
-  // Final posting of data
-  amounts[index + 1].innerHTML += output;
 }
+
 
 //Indexing agent for matching asynchronous replies to page
 function indexFinder(coinName) {
--- popup.js
+++ popup.js
@@ -1,37 +1,34 @@
 /*jshint esversion: 6 */
-console.log("pop");
 function saveCurrency(val) {
-  console.log("save atempt");
   var data = {};
   data.currency = val;
   chrome.storage.sync.set(data);
 }
 
 function changeCurrency() {
-  console.log("pop1s");
-  chrome.tabs.executeScript(null, {file: "content.js"});
+  chrome.tabs.executeScript(null, {file: "content.js"}, () => {
+    //chrome.tabs.executeScript(null, {file: "tv.js"});
+  });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+chrome.tabs.executeScript(null, {file: "jquery-3.2.1.min.js"});
 
-  changeCurrency();
+document.addEventListener('DOMContentLoaded', () => {
 
   var dropdown = document.getElementById('dropdown');
 
   chrome.storage.sync.get('currency', (data) => {
     if (typeof data.currency === 'undefined') {
       saveCurrency(dropdown.value);
-      console.log('24');
     } else if (dropdown.value != data.currency) {
       dropdown.value = data.currency;
-      console.log('27');
     }
-    console.log(currency);
     changeCurrency(data.currency);
-  });
 
-  dropdown.addEventListener('change', () => {
-    changeCurrency(dropdown.value);
-    saveCurrency(dropdown.value);
+
+    dropdown.addEventListener('change', () => {
+      changeCurrency(dropdown.value);
+      saveCurrency(dropdown.value);
+    });
   });
 });
