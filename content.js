0e5fb19491349d8f6dfdfe5456be6df36814e714
Cody Neiman <neiman1010222@gmail.com>
2018-01-04 22:16:00 -0500

    Added link to cryptocompare for each value and made easier to read

--- content.js
+++ content.js
@@ -13,18 +13,38 @@ var HttpClient = function() {
     };
 };
 
+function getCurrency(currency) {
+const symbols = {
+  USD: '$',
+  EUR: '€',
+  GBP: '£',
+  INR: '₹',
+  AUD: 'A$',
+  CAD: 'C$',
+  JPY: 'J￥',
+  CNY: 'C￥'
+};
+return symbols[currency];
+}
+
 // Collection of website information by class
 var names = document.getElementsByClassName('coin ng-binding');
 var amounts = document.getElementsByClassName('total f-right ng-binding');
 var btcs = document.getElementsByClassName('equalValue f-right ng-binding');
 var namesIndex = [];
+var index;
 var currency = 'USD';
+var prefix = `<br><button class="btn btn-success" onclick=" window.open('https://www.cryptocompare.com/coins/`;
+var suffix0 = `/overview/`;
+var suffix1 = `','_blank')">`;
+var suffix2 = `</button>`;
 
 // Test if prices have already been added
-if (amounts[1].innerHTML.includes('-')) {
+if (amounts[1].innerHTML.includes('<button class="btn btn-success"')) {
   for (var i = 1; i < amounts.length; i++) {
     // Removal of previous prices
     amounts[i].innerHTML = amounts[i].innerHTML.substring(0, amounts[i].innerHTML.indexOf('-'));
+    amounts[i].innerHTML = amounts[i].innerHTML.replace('<br>', '');
   }
 }
 
@@ -39,6 +59,7 @@ chrome.storage.sync.get('currency', (data) => {
     requestion(request, names[i].innerHTML);
   }
 });
+
 // Asynchronous calling
 function requestion(request, name) {
   var client = new HttpClient();
@@ -51,32 +72,18 @@ function requestion(request, name) {
 function parseP(coinName, coinValue) {
   coinValue = coinValue.substr(7);
   coinValue = coinValue.substr(0, coinValue.length - 1);
-  //console.log('DEBUG: Coin Name: ' + coinName.replace(/<(?:.|\n)*?>/gm, ''));
-  //console.log('DEBUG: Coin Value: ' + parseFloat(coinValue));
-  var index = indexFinder(coinName);
-  //console.log('DEBUG: Coin Amounts: ' + parseFloat(amounts[index].innerHTML));
-  var output = parseFloat(coinValue) * parseFloat(btcs[index + 1].innerHTML);
+  index = indexFinder(coinName);
+  if (parseFloat(btcs[index + 1].innerHTML) > 0) {
+    var output = parseFloat(coinValue) * parseFloat(btcs[index + 1].innerHTML);
 
-  function getCurrency(currency) {
-  const symbols = {
-    USD: '$',
-    EUR: '€',
-    GBP: '£',
-    INR: '₹',
-    AUD: 'A$',
-    CAD: 'C$',
-    JPY: 'J￥',
-    CNY: 'C￥'
-  };
-  return symbols[currency];
+    output = getCurrency(currency) + output.toFixed(2);
+    //console.log('TOTAL: ' + output);
+    // To-do: Add tradingview graphs to changed elements
+    // Final posting of data
+    amounts[index + 1].innerHTML += prefix + coinName.replace(/<(?:.|\n)*?>/gm, '').toLowerCase() + suffix0 + currency + suffix1 + output + suffix2;
   }
-
-  output = ' - ' + getCurrency(currency) + output.toFixed(2);
-  //console.log('TOTAL: ' + output);
-  // To-do: Add tradingview graphs to changed elements
-  // Final posting of data
-  amounts[index + 1].innerHTML += output;
 }
+
 
 //Indexing agent for matching asynchronous replies to page
 function indexFinder(coinName) {
